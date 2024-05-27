14ef4e05b74ae76daf1aab74dfa631fb91805781
samiakhlaq <mdakhlaq2345@gmail.com>
2024-05-27 16:18:37 +0600

    705A - Hulk

---
+++ Hulk.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class Hulk {
+    public static void express (int n)
+    {
+        int i = 0;
+        while(i<n)
+        {
+            if (i%2 == 0)
+            {
+                System.out.print("I hate ");
+            }
+            else{
+                System.out.print("I love ");
+            }
+            if(i<n-1)
+            {
+                System.out.print("that ");
+            }
+            else{
+                System.out.print("it");
+            }
+            i++;
+        }
+    }
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            int n = sc.nextInt();
+            express(n);
+        }
+    }
+}
